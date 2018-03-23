5c39182e8568256139d971d3171593fb15003c6c
stanislavlyalin <lsg@znak.ru>
2018-03-23 16:04:13 +0300

    доработано событиями окно регистрации

--- signup_window.py
+++ signup_window.py
@@ -4,11 +4,15 @@ from PyQt5.QtWidgets import QDialog, QHBoxLayout, QVBoxLayout, QLabel, QPushButt
 from PyQt5 import QtCore
 from phrases import randomPhrase
 
+ENABLED_STYLE = 'color: green; border: 1px solid green; border-radius: 5px'
+DISABLED_STYLE = 'color: grey; border: 1px solid grey; border-radius: 5px'
+
 # форма добавления нового пользователя
 class SignUpWindow(QDialog):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Зарегистрироваться в системе')
+        self.attempts = 0
 
         self.vBox = QVBoxLayout()
         self.attemptsBox = QHBoxLayout()
@@ -20,6 +24,7 @@ class SignUpWindow(QDialog):
         self.attemptLabels = [QLabel(str(i+1)) for i in range(3)]
         self.startButton = QPushButton('Старт')
         self.stopButton = QPushButton('Стоп')
+        self.stopButton.setEnabled(False)
         self.textToSpeech = QLabel('Текст для произнесения')
         self.readyButton = QPushButton('Готово')
 
@@ -28,7 +33,7 @@ class SignUpWindow(QDialog):
         self.vBox.addWidget(QLineEdit('Ваше имя'))
         self.vBox.addWidget(QLabel('Запишите 3 фрагмента речи'))
         for i in range(3):
-            self.attemptLabels[i].setStyleSheet('color: grey; border: 1px solid grey; border-radius: 5px')
+            self.attemptLabels[i].setStyleSheet(DISABLED_STYLE)
             self.attemptLabels[i].setAlignment(QtCore.Qt.AlignCenter)
             self.attemptLabels[i].setEnabled(False)
             self.attemptsBox.addWidget(self.attemptLabels[i])
@@ -45,7 +50,21 @@ class SignUpWindow(QDialog):
         self.readyBox.addWidget(self.readyButton)
         self.vBox.addLayout(self.readyBox)
 
-        self.stopButton.clicked.connect(lambda: self.textToSpeech.setText(randomPhrase()))
+        # connect
+        self.startButton.clicked.connect(lambda: self.startButtonCliked())
+        self.stopButton.clicked.connect(lambda: self.stopButtonCliked())
 
     def showEvent(self, event):
         self.textToSpeech.setText(randomPhrase())
+
+    def startButtonCliked(self):
+        self.stopButton.setEnabled(True)
+        self.startButton.setEnabled(False)
+
+    def stopButtonCliked(self):
+        self.attemptLabels[self.attempts].setStyleSheet(ENABLED_STYLE)
+        self.attempts += 1
+        self.stopButton.setEnabled(False)
+        self.startButton.setEnabled(True if self.attempts < 3 else False)
+        self.textToSpeech.setText(randomPhrase())
+        
