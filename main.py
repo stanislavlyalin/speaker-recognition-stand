bc16b321a324ae8474aafb3eaeefa16a2b9cae18
stanislavlyalin <lyalinstas@gmail.com>
2018-03-23 11:26:20 +0300

    интерфейс окна регистрации нового пользователя

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QDialog, QHBoxLayout, QVBoxLayout, QLabel, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QDialog, QHBoxLayout, QVBoxLayout, QLabel, QPushButton, QLineEdit, QSpacerItem
 from PyQt5.QtGui import QPixmap
+from PyQt5 import QtCore
 
 class StaticImage(QLabel):
     def __init__(self, path):
@@ -14,6 +15,41 @@ class StaticImage(QLabel):
 class SignUpWindow(QDialog):
     def __init__(self):
         super().__init__()
+        self.vBox = QVBoxLayout()
+        self.attemptsBox = QHBoxLayout()
+        self.buttonsBox = QHBoxLayout()
+        self.readyBox = QHBoxLayout()
+        self.setLayout(self.vBox)
+
+        # создание компонентов
+        self.attemptLabels = [QLabel(str(i+1)) for i in range(3)]
+        self.startButton = QPushButton('Старт')
+        self.stopButton = QPushButton('Стоп')
+        self.textToSpeech = QLabel('Текст для произнесения')
+        self.readyButton = QPushButton('Готово')
+
+        # расположение компонентов на форме
+        self.vBox.addWidget(QLabel('Представьтесь'))
+        self.vBox.addWidget(QLineEdit('Ваше имя'))
+        self.vBox.addWidget(QLabel('Запишите 3 фрагмента речи'))
+        for i in range(3):
+            self.attemptLabels[i].setStyleSheet('color: grey; border: 1px solid grey; border-radius: 5px')
+            self.attemptLabels[i].setAlignment(QtCore.Qt.AlignCenter)
+            self.attemptLabels[i].setEnabled(False)
+            self.attemptsBox.addWidget(self.attemptLabels[i])
+        self.vBox.addLayout(self.attemptsBox)
+        self.vBox.addWidget(self.textToSpeech)
+
+        self.buttonsBox.addStretch(1)
+        self.buttonsBox.addWidget(self.startButton)
+        self.buttonsBox.addWidget(self.stopButton)
+        self.buttonsBox.addStretch(1)
+        self.vBox.addLayout(self.buttonsBox)
+
+        self.readyBox.addStretch(1)
+        self.readyBox.addWidget(self.readyButton)
+        self.vBox.addLayout(self.readyBox)
+
 
 # форма голосовой идентификации
 class IdentifyWindow(QDialog):
